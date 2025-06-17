6ca7c99ebe5a38e74597a860e3a74535b523b3e0
bella234428 <ibseali258@gmail.com>
2025-06-17 14:47:30 +0300

    add more code

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const guestList = document.getElementById("guest-list");
 
 function addTask(){
     if(guestName.value === ''){
-        alert("You must right something!")
+        alert("You must right something!");
     }
+     else{
+        let li = document.createElement("li");
+        li.innerHTML = guestName.value;
+        guestList.appendChild(li);
+     }
 }
